270129de27eea2781ff35afb9f87bcfd3aac8a0f
Baptiste Gentholz <baptiste.gentholz@gmail.com>
2017-11-19 19:10:30 +0100

    Remove useless comments

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,20 +2,7 @@
  * Created by thomashourlier on 2/26/17.
  */
 
-// import axios from 'axios';
 import { ADD_NEW_JEDI, FETCH_JEDI, JEDI_ERROR, JEDI_RECEIVED } from './constants';
-
-// export function fetchJedi() {
-//   return (dispatch) => {
-//     axios.get('http://localhost:3001/jedi')
-//       .then((res) => {
-//         dispatch({
-//           type: 'FETCH_FINISH',
-//           payload: res.data,
-//         });
-//       });
-//   };
-// }
 
 export const fetchJedi = () =>
   ({
